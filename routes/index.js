337c9665e716b17c4a4f9482dfa744e24aeaa901
Willdooo <wollmada@gmail.com>
2021-09-14 14:57:02 +0200

    Final commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const moment = require("moment");
+
+// let date = moment().format("YYYY-MM-DD");
+// console.log(date);
+//messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: moment().format("LLL"),
+    title: "Good day!",
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: moment().format("LLL"),
+    title: "Greetings!",
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+//posting messages to index (homepage) view
+//-> after getting called from form.pug         form(action="/new", method="POST")
+
+router.post("/new", (req, res, next) => {
+  const newMessage = {
+    user: req.body.author,
+    title: req.body.title,
+    text: req.body.text,
+    added: moment().format("LLL"),
+  };
+  console.log(newMessage);
+  messages.unshift(newMessage);
+  console.log(messages);
+  res.redirect("/");
 });
 
 module.exports = router;
